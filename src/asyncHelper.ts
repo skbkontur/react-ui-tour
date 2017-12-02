3c47e2de05a0050727a94706c6ac309945558564
Vladimir Tolstikov <wertyxa@bk.ru>
2017-12-03 00:29:33 +0500

    removed deprecated code

--- src/asyncHelper.ts
+++
@@ -1,9 +0,0 @@
-import {ThunkAction} from 'redux-thunk';
-import {InitialState} from './reducers';
-import {AxiosStatic} from 'axios';
-
-export type ExtraArgument = AxiosStatic;
-
-export interface AsyncAction<T = void> {
-  (...arg: any[]): ThunkAction<Promise<T>, InitialState, ExtraArgument>;
-}
