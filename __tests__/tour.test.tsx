e1cbaf43ccf171f2c51cb737b4134a08f29233f4
Ivan Shestakov <i.shestakov@kontur>
2017-12-27 13:46:48 +0300

    onTourShown behavior was tested

--- __tests__/tour.test.tsx
+++ __tests__/tour.test.tsx
@@ -6,38 +6,51 @@ import {Tour} from '../src/tour/tour';
 
 describe('test tour main logic', () => {
   const tourId = 'id1';
+  const tourSecondId = 'id2';
   const predicateFunc = jest.fn();
-  const onTourShownFunc = jest.fn(id => `shown tour ${id}`);
-  predicateFunc.mockReturnValue(true);
-  let providerWrapper;
+  let providerWrapper, onTourShownFunc;
 
   beforeEach(() => {
+    onTourShownFunc = jest.fn(id => `shown tour ${id}`);
+    predicateFunc.mockReturnValue(true);
+
     providerWrapper = mount(<TourProvider
       predicate={predicateFunc}
       onTourShown={onTourShownFunc}
     >
-      <Tour id={tourId}/>
+      <div>
+        <Tour id={tourId}/>
+        <Tour id={tourSecondId}/>
+      </div>
     </TourProvider>);
   });
 
   it('tour was successfully mounted and unmounted', () => {
-    const tourCmp = providerWrapper.find(Tour);
+    const tourWrapper = providerWrapper.find(Tour).first();
 
-    expect(tourCmp.instance().state.active).toEqual(true);
-    tourCmp.instance().componentWillUnmount();
+    expect(tourWrapper.instance().state.active).toEqual(true);
+    tourWrapper.instance().componentWillUnmount();
     expect(providerWrapper.is(Tour)).toEqual(false);
-    expect(onTourShownFunc).toBeCalledWith(tourId);
   });
 
   it('provider\'s subscribe and unsubscribe were successfully called', () => {
-    const providerInstance: any = providerWrapper.instance();
-    const tourCmp = providerWrapper.find(Tour);
+    const providerCmp = providerWrapper.instance();
+    const tourWrapper = providerWrapper.find(Tour).first();
 
     expect(predicateFunc).toBeCalled();
-    expect(tourCmp.prop('id')).toEqual(tourId);
-    expect(providerInstance.listeners[tourId]).toBeDefined();
-    expect(providerInstance.currentId).toEqual(tourId);
-    tourCmp.instance().componentWillUnmount();
-    expect(providerInstance.listeners[tourId]).toBeUndefined();
+    expect(tourWrapper.prop('id')).toEqual(tourId);
+    expect(providerCmp.listeners[tourId]).toBeDefined();
+    expect(providerCmp.currentId).toEqual(tourId);
+    tourWrapper.instance().unsubscribe(true);
+    expect(providerCmp.listeners[tourId]).toBeUndefined();
+  });
+
+  it('onTourShown was called only if tour was displayed', () => {
+    const firstTourWrapper = providerWrapper.find(`#${tourId}`);
+    const secondTourWrapper = providerWrapper.find(`#${tourSecondId}`);
+    firstTourWrapper.instance().unsubscribe(true);
+    secondTourWrapper.instance().componentWillUnmount();
+
+    expect(onTourShownFunc).toHaveBeenCalledTimes(1);
   });
 });
