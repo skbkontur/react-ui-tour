c3c8407789dfb4fa1b4794f152e00806859b36d8
Ivan Shestakov <i.shestakov@kontur>
2017-12-27 13:47:38 +0300

    example fixes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,6 @@ import {render} from 'react-dom';
 
 import {Provider} from 'react-redux';
 import {initStore} from './initStore';
-import Example from '../exampleFeature';
 import {Tour} from '../tour/tour';
 import {TourProvider} from '../tour/tourProvider';
 import {ModalStep} from '../steps/modalStep';
@@ -34,10 +33,10 @@ export function showReact(initialState, reactContainer) {
   render((
     <Provider store={store}>
       <TourProvider predicate={(id) => true}
-                    onTourShown={(id) => console.log('shown tour' + id)}>
+                    onTourShown={(id) => console.log(`shown tour ${id}`)}>
         <div>
-          <Tour id="id1">
-            <ModalStep header="modal header" content="modal content"/>
+          <Tour id='id1'>
+            <ModalStep header='modal header' content='modal content'/>
             <TooltipStep
               tooltipTarget={tooltipTarget2}
               highlightTarget={highlightTarget}
@@ -51,15 +50,13 @@ export function showReact(initialState, reactContainer) {
               render={CustomStep}
               tooltipPosition='right top'
             />
-            {/*<TooltipStep tooltipTarget={tooltipTarget2} highlightTarget={highlightTarget} header="First slide"*/}
-                {/*content={<div>some content<br/>another content</div>}/>*/}
-            {/*<TooltipStep element={element2} header="Second slide"/>*/}
-            {/*<TooltipStep element={element3} header="Third slide"/>*/}
-            {/*<TooltipStep element={element3} header="Fin slide" final/>*/}
           </Tour>
-          {/*<Tour id="id2">*/}
-            {/*<TooltipStep element={element3} header="Second tour start"/>*/}
-          {/*</Tour>*/}
+
+          <div>
+            <Tour id='id2'>
+              <ModalStep header='modal header 2' content='modal content 2'/>
+            </Tour>
+          </div>
         </div>
       </TourProvider>
     </Provider>
