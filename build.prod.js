98c83510fddcd852e5ad576abd84a9bd6c0da32c
Ivan Shestakov <i.shestakov@kontur>
2018-01-15 20:38:24 +0300

    build folder will be deleted before new copy

--- build.prod.js
+++ build.prod.js
@@ -1,24 +1,38 @@
-var ncp = require('ncp').ncp
-const path = require('path')
+var fs = require('fs');
+var ncp = require('ncp').ncp;
+const path = require('path');
 
-ncp.limit = 1000
+ncp.limit = 1000;
 
-var source = path.join(__dirname, 'src/lib')
-var destination = path.join(__dirname, 'build')
+var source = path.join(__dirname, './src/lib');
+var destination = path.join(__dirname, './build');
 var options = {
   filter: /^[A-Za-z\-\/]*(\.less|$)/
-}
+};
+
+(function deleteFolderRecursive (path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function (file, index) {
+      var curPath = path + '/' + file;
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    })
+    fs.rmdirSync(path);
+  }
+})(destination);
 
 ncp(source, destination, options, function (err) {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
 })
 
-var exec = require('child_process').exec
-var child = exec('tsc',
-  function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log('tsc error: ' + error)
-    }
-  })
+var exec = require('child_process').exec;
+var child = exec('tsc', function (error, stdout, stderr) {
+  if (error !== null) {
+    console.log('tsc error: ' + error);
+  }
+})
