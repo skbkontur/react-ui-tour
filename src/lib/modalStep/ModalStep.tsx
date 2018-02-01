1ba551b44138f3311671a00da8419903d51d2c9a
Vladimir Tolstikov <wertyxa@bk.ru>
2018-02-01 14:12:20 +0500

    made render and footer props of model step function

--- src/lib/modalStep/ModalStep.tsx
+++ src/lib/modalStep/ModalStep.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import Modal from '@skbkontur/react-ui/components/Modal';
-import Button from '@skbkontur/react-ui/components/Button';
+import * as React from "react";
+import Modal from "@skbkontur/react-ui/components/Modal";
+import Button from "@skbkontur/react-ui/components/Button";
 
-import {StepProps, StepInternalProps} from '../tour/Tour'
+import { StepProps, StepInternalProps } from "../tour/Tour";
 
 export interface ModalStepOuterProps {
   width?: number;
@@ -12,21 +12,45 @@ export interface ModalStepOuterProps {
   render?: (props: StepInternalProps) => React.ReactElement<any>;
 }
 
-export interface ModalStepProps extends ModalStepOuterProps, StepProps, Partial<StepInternalProps> {}
+export interface ModalStepProps
+  extends ModalStepOuterProps,
+    StepProps,
+    Partial<StepInternalProps> {}
 
 export class ModalStep extends React.Component<ModalStepProps, {}> {
   render() {
-    const {header, content
-      , footer, onNext
-      , width, onPrev, onClose} = this.props as ModalStepProps & StepInternalProps;
+    const {
+      header,
+      content,
+      footer,
+      onNext,
+      width,
+      onPrev,
+      onClose,
+      render,
+      stepIndex,
+      stepsCount
+    } = this.props as ModalStepProps & StepInternalProps;
     return (
       <Modal onClose={onClose} width={width}>
-        <Modal.Header>{header}</Modal.Header>
-        <Modal.Body>{content}</Modal.Body>
-        <Modal.Footer>
-          <Button use="primary" onClick={onNext}>Поехали</Button>
-        </Modal.Footer>
+        {render ? (
+          render({ onNext, onPrev, onClose, stepIndex, stepsCount })
+        ) : (
+          <div>
+            <Modal.Header>{header}</Modal.Header>
+            <Modal.Body>{content}</Modal.Body>
+            {footer ? (
+              footer({ onNext, onPrev, onClose, stepIndex, stepsCount })
+            ) : (
+              <Modal.Footer>
+                <Button use="primary" onClick={onNext}>
+                  Поехали
+                </Button>
+              </Modal.Footer>
+            )}
+          </div>
+        )}
       </Modal>
-    )
+    );
   }
 }
