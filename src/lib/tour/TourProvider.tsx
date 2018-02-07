1b9759590cbf519c398c5643cc2bdc7d69c00c33
Vladimir Tolstikov <wertyxa@bk.ru>
2018-02-07 11:11:34 +0500

    made props of TourProvider optional

--- src/lib/tour/TourProvider.tsx
+++ src/lib/tour/TourProvider.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 export interface TourProviderProps {
-  predicate: (id: string) => boolean;
-  onTourShown: (id: string) => void;
+  predicate?: (id: string) => boolean;
+  onTourShown?: (id: string) => void;
 }
 
 export class TourProvider extends React.Component<TourProviderProps, {}> {
@@ -11,7 +11,7 @@ export class TourProvider extends React.Component<TourProviderProps, {}> {
     [TourProvider.contextName]: React.PropTypes.object.isRequired,
   };
 
-  currentId: string;
+  currentId: string | undefined;
   queue = [] as string[];
   listeners: {
     [id: string]: () => void;
@@ -41,7 +41,12 @@ export class TourProvider extends React.Component<TourProviderProps, {}> {
     delete this.listeners[id];
   }
 
-  onShown = (id) => this.props.onTourShown(id);
+  onShown = (id) => this.props.onTourShown && this.props.onTourShown(id);
+
+  idPredicate = (id) => {
+    const predicate = this.props.predicate;
+    return predicate ? predicate(id) : true;
+  }
 
   notify(id) {
     this.listeners[id]();
@@ -49,7 +54,7 @@ export class TourProvider extends React.Component<TourProviderProps, {}> {
 
   removeFromQueue(id) {
     if (id !== this.currentId) return;
-    this.currentId = this.queue.find(id => this.props.predicate(id));
+    this.currentId = this.queue.find(this.idPredicate);
     this.queue = this.queue.filter(id => id !== this.currentId);
     if (this.currentId) {
       this.notify(this.currentId);
@@ -58,7 +63,7 @@ export class TourProvider extends React.Component<TourProviderProps, {}> {
 
   pushToQueue(id) {
     this.queue = this.currentId ? this.queue.concat(id) : this.queue;
-    if (!this.currentId && this.props.predicate(id)) {
+    if (!this.currentId && this.idPredicate(id)) {
       this.currentId = id;
       this.notify(id);
     }
