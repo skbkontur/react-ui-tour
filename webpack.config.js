562137ab58226b4bc18e3ab2b2cf162e69d7c258
Ivan Shestakov <--global>
2018-04-12 17:22:35 +0300

    update webpack version & configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,141 @@
 'use strict';
 
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
-const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-var es3ifyPlugin = require('es3ify-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-let production = false;
-for (let i = 2; i < process.argv.length; i++){
-  if (/-p/.test(process.argv[i])){
-    production = true;
-    break;
-  }
-}
+const PATHS = {
+  app: path.resolve(__dirname, './src/example/app.tsx'),
+  dist: path.resolve(__dirname, './dist'),
+  htmlTemplate: path.resolve(__dirname, './index.html'),
+  external: path.resolve(__dirname, './node_modules')
+};
 
-//todo: turn off warnings
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: {
-    app: './example/app',
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: production ? 'http://tech.skbkontur.ru/react-ui-tour/' : '/dist/',
-    filename: production ? '[name].[hash].js' : '[name].js',
-    library: ['[name]'],
-    libraryTarget: 'umd',
-  },
+const devServer = ({ host, port } = {}) => ({
+  host,
+  port,
+  open: true,
+  historyApiFallback: true
+});
+
+const loadTS = ({ include, exclude } = {}) => ({
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader?useCache=true',
-      },
+        loader: 'awesome-typescript-loader',
+        include,
+        exclude,
+        options: {
+          useCache: true,
+          forceIsolatedModules: true
+        }
+      }
+    ]
+  }
+});
+
+const extractCSS = (loaderOptions = {}, filename = '[name].css') => {
+  const plugin = new ExtractTextPlugin({
+    allChunks: true,
+    filename: filename
+  });
+  loaderOptions.use = loaderOptions.use || 'css-loader';
+
+  return {
+    module: {
+      rules: [
+        Object.assign({ test: /\.css$/ }, loaderOptions, {
+          use: plugin.extract({
+            use: loaderOptions.use,
+            fallback: 'style-loader'
+          })
+        })
+      ]
+    },
+    plugins: [plugin]
+  };
+};
+
+const cssModulePrefix = 'mcss';
+
+const extractLESS = (options = {}) =>
+  extractCSS(
+    Object.assign(
       {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[name]__[local]#[md5:hash:hex:4]'),
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: `${cssModulePrefix}-[path][name]-[local]`
+            }
+          },
+          'less-loader'
+        ]
       },
+      options
+    )
+  );
+
+const emitSourceMaps = () => ({
+  module: {
+    rules: [
       {
-        test: /\.less/,
-        loader: ExtractTextPlugin.extract('typings-for-css-modules-loader?localIdentName=[name]__[local]#[md5:hash:hex:4]&modules&less&namedExport'),
-      },
-      {test: /\.less$/, loader: 'less-loader'},
-      {test: /\.(png|woff|woff2|eot)$/, loader: 'file-loader?name=[name].[md5:hash:hex:8].[ext]'},
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'source-map-loader'
+      }
+    ]
+  }
+});
+
+module.exports = merge(
+  {
+    entry: { app: PATHS.app },
+    output: {
+      path: PATHS.dist,
+      filename: '[name].bundle.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.css']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: PATHS.htmlTemplate }),
+      new HardSourceWebpackPlugin(),
+      new webpack.WatchIgnorePlugin([/less\.d\.ts$/])
     ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|woff|woff2|eot)$/,
+          loader: 'file-loader?name=[name].[md5:hash:hex:8].[ext]'
+        }
+      ]
+    }
   },
-  resolve: {
-    extensions: ['', '.ts', '.tsx', '.js'],
-  },
-  plugins: [
-    new webpack.WatchIgnorePlugin([ /less\.d\.ts$/ ]),
-    new ExtractTextPlugin(production ? '[name].[hash].css' : '[name].css'),
-    new HtmlWebpackPlugin(),
-    new WebpackCleanupPlugin({
-      exclude: ['.git/**/*', '.*']
-    })
-  ],
-  devtool: production ? null : 'inline-source-map',
-}
+  loadTS(),
+  extractCSS({ include: [path.resolve(PATHS.external, '.')] }, 'vendor.css'),
+  extractLESS({ exclude: /[\\/]node_modules[\\/]/ }),
+  emitSourceMaps(),
+  {
+    devtool: 'source-map',
+    devServer: devServer(),
+    mode: 'development'
+  }
+);
