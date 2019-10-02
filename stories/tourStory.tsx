30ae19b4fd829c1253e28b113060735f92b28c60
ikuleshova <ikuleshova@skbkontur.ru>
2019-10-02 10:48:07 +0500

    Revert "Modify story (TODO remove)"
    This reverts commit 26dcc55c56dfc164794f14ed82152fac9c43968b.

--- stories/tourStory.tsx
+++ stories/tourStory.tsx
@@ -1,33 +1,22 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { TooltipStep, ModalStep } from "../src/lib";
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {TooltipStep, ModalStep} from '../src/lib';
 
-storiesOf("Tour", module)
-  .add("tooltip step", () => (
+storiesOf('Tour', module)
+  .add('tooltip step', () => (
     <TooltipStep
-      header="hi there"
+      header='hi there'
       target={() => document.documentElement}
-      positions={["bottom left"]}
-      onNext={action("next")}
-      onPrev={action("prev")}
-      onClose={action("close")}
+      positions={['bottom left']}
+      onNext={action('next')}
+      onPrev={action('prev')}
+      onClose={action('close')}
       stepIndex={1}
       stepsCount={3}
-      width={900}
-      content={
-        "qwieuhflqwieufhuwqefh luwheflqwuehfleuwq huefq;weufh ;qwuefhqwiefywqueyfg\n" +
-        "      quywefglq iwuehf qqlwieufhlq wiuefqqlifuhq lwiufh qqluifhq liuewfh\n" +
-        "      qwqiuefhlq uwefhl quwheqliuwehflqiuwhefuiq ehfqliweufhl qiuheflquwhf\n" +
-        "      qlehfu lqfuh"
-      }
     />
   ))
-  .add("modal step", () => (
-    <ModalStep
-      header="hi there"
-      onNext={action("next")}
-      onClose={action("onclose")}
-    />
+  .add('modal step', () => (
+    <ModalStep header='hi there' onNext={action('next')} onClose={action('onclose')}/>
   ));
