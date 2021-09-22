bd06f10b7ce5ff0f1904e2c8ba7942d91c068cd8
siu <siu@skbkontur.ru>
2021-09-22 14:00:25 +0500

    fixed indents in story

--- stories/tourStory.tsx
+++ stories/tourStory.tsx
@@ -5,25 +5,25 @@ import {TooltipStep, ModalStep} from '../src/lib';
 import {MiniTooltipStep} from "../src/lib";
 
 storiesOf('Tour', module)
-    .add('tooltip step', () => (
-        <TooltipStep
-            header='hi there'
-            target={() => document.documentElement}
-            positions={['bottom left']}
-            onNext={action('next')}
-            onPrev={action('prev')}
-            onClose={action('close')}
-            stepIndex={1}
-            stepsCount={3}
-        />
-    ))
-    .add('mini tooltip step', () => (
-        <MiniTooltipStep
-            targetGetter={() => document.documentElement}
-            positions={["bottom left"]}>
+  .add('tooltip step', () => (
+      <TooltipStep
+          header='hi there'
+          target={() => document.documentElement}
+          positions={['bottom left']}
+          onNext={action('next')}
+          onPrev={action('prev')}
+          onClose={action('close')}
+          stepIndex={1}
+          stepsCount={3}
+      />
+  ))
+  .add('mini tooltip step', () => (
+      <MiniTooltipStep
+          targetGetter={() => document.documentElement}
+          positions={["bottom left"]}>
             hi there
-        </MiniTooltipStep>
-    ))
-    .add('modal step', () => (
-        <ModalStep header='hi there' onNext={action('next')} onClose={action('onclose')}/>
-    ));
+      </MiniTooltipStep>
+  ))
+  .add('modal step', () => (
+      <ModalStep header='hi there' onNext={action('next')} onClose={action('onclose')}/>
+  ));
