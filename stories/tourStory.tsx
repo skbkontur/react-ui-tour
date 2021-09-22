ccb6f5486514d7810606a35117fc99bd3fa53a56
siu <siu@skbkontur.ru>
2021-09-22 14:01:35 +0500

    indent fix 2

--- stories/tourStory.tsx
+++ stories/tourStory.tsx
@@ -6,24 +6,24 @@ import {MiniTooltipStep} from "../src/lib";
 
 storiesOf('Tour', module)
   .add('tooltip step', () => (
-      <TooltipStep
-          header='hi there'
-          target={() => document.documentElement}
-          positions={['bottom left']}
-          onNext={action('next')}
-          onPrev={action('prev')}
-          onClose={action('close')}
-          stepIndex={1}
-          stepsCount={3}
-      />
+    <TooltipStep
+      header='hi there'
+      target={() => document.documentElement}
+      positions={['bottom left']}
+      onNext={action('next')}
+      onPrev={action('prev')}
+      onClose={action('close')}
+      stepIndex={1}
+      stepsCount={3}
+    />
   ))
   .add('mini tooltip step', () => (
-      <MiniTooltipStep
-          targetGetter={() => document.documentElement}
-          positions={["bottom left"]}>
-            hi there
-      </MiniTooltipStep>
+    <MiniTooltipStep
+      targetGetter={() => document.documentElement}
+      positions={["bottom left"]}>
+        hi there
+    </MiniTooltipStep>
   ))
   .add('modal step', () => (
-      <ModalStep header='hi there' onNext={action('next')} onClose={action('onclose')}/>
+    <ModalStep header='hi there' onNext={action('next')} onClose={action('onclose')}/>
   ));
